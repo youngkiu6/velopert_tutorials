7f703087d8c7d92806ab414c8dd5df16edb7c613
류영기 <youngkiu@lyuyeong-giui-MacBookPro.local>
2020-04-17 18:57:58 +0900

    3. 컴포넌트 최적화

--- src/App.js
+++ src/App.js
@@ -45,21 +45,19 @@ class App extends Component {
 
   handleToggle = (id) => {
     const { todos } = this.state;
-
-    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
     const index = todos.findIndex(todo => todo.id === id);
-    const selected = todos[index]; // 선택한 객체
 
-    const nextTodos = [...todos]; // 배열을 복사
-
-    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
-    nextTodos[index] = {
-      ...selected,
-      checked: !selected.checked
-    };
+    const selected = todos[index];
 
     this.setState({
-      todos: nextTodos
+      todos: [
+        ...todos.slice(0, index),
+        {
+          ...selected,
+          checked: !selected.checked
+        },
+        ...todos.slice(index + 1, todos.length)
+      ]
     });
   }
 
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import './TodoItem.css';
 
 class TodoItem extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.checked !== nextProps.checked;
+    }
+
     render() {
         const { text, checked, id, onToggle, onRemove } = this.props;
+
+        console.log(id);
 
         return (
             <div className="todo-item" onClick={() => onToggle(id)}>
