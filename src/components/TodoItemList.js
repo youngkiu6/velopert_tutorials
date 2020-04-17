7dbb7587304246ca8f233c8e763d3c9d305f1016
류영기 <youngkiu@lyuyeong-giui-MacBookPro.local>
2020-04-17 17:00:59 +0900

    네번째 컴포넌트, TodoItem 컴포넌트 만들기

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
     render() {
@@ -6,7 +7,9 @@ class TodoItemList extends Component {
 
         return (
             <div>
-
+                <TodoItem text="안녕" />
+                <TodoItem text="리액트" />
+                <TodoItem text="반가워" />
             </div>
         );
     }
