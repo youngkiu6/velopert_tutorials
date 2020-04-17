fa0ae751224907da4298b3c9071788c7f7a597e7
youngkiu6 <youngkiu6@gmail.com>
2020-04-17 11:34:30 +0900

    데이터 수정

--- src/App.js
+++ src/App.js
@@ -31,6 +31,16 @@ class App extends Component {
       information: information.filter(info => info.id !== id)
     })
   }
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+          ? { ...info, ...data } // 새 객체를 만들어서 기존의 값과 전달받은 data 을 덮어씀
+          : info // 기존의 값을 그대로 유지
+      )
+    })
+  }
   render() {
     const { information } = this.state;
     return (
@@ -41,6 +51,7 @@ class App extends Component {
         <PhoneInfoList
           data={information}
           onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
         />
       </div>
     );
