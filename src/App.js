42b3de2e8c356c3b594865699912842b363286e5
류영기 <youngkiu@lyuyeong-giui-MacBookPro.local>
2020-04-17 16:04:53 +0900

    첫번째 컴포넌트, TodoTemplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <TodoListTemplate form={<div>이렇게 말이죠.</div>}>
+        <div>여기엔 children 자리구요.</div>
+      </TodoListTemplate>
     );
   }
 }
