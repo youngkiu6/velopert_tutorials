bf70af678245f7dd4ee7a5199a05bdc9574e61fc
youngkiu6 <youngkiu6@gmail.com>
2020-04-22 11:28:15 +0900

    Actions, Reducers, Store 만들기

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
+// Redux 관련 불러오기
+import { createStore } from 'redux'
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+// 스토어 생성
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
